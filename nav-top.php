c357d1b80d93b2fb81faa5412a524338517c23cc
dealtek <dealtek@gmail.com>
2016-03-01 17:13:57 -0800

    v1

---
+++ nav-top.php
@@ -0,0 +1,48 @@
+<table  border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><img name="logo" src="../store-images/AEPL-logo-small.png" width="133" height="60" alt="" /></td>
+    <td><div class="wrap">
+        <div class="demo-container">
+          <div class="white">
+            <ul id="mega-menu-9" class="mega-menu">
+              <li><a href="../../index.php">Home</a></li>
+              <li><a href="../../about.php">About Us</a>
+              
+              <ul>
+              <li><a href="../../about.php#story">Our Story</a></li>
+                  <li><a href="../../about.php#assets">Company Assets</a></li>
+                  <!-- --><li><a href="../../contact-us.php">Contact Us</a></li>
+                </ul>
+              
+              </li>
+              <li><a href="../../current-project.php">Current Project</a></li>
+               <li><a href="../../contact-us.php">Contact Us</a></li>
+              <li><a href="#" class="MenuBarItemSubmenu">Investors</a>
+                <ul>
+                  <li><a href="../customers/ct-details.php">My Details</a></li>
+                  <li><a href="../customers/login-customer.php">Login</a></li>
+                  <li><a href="../customers/ct-register-start.php">Register</a></li>
+                  <li><a href="../customers/logout-customer.php">Log Out</a></li>
+                </ul>
+              </li>
+             
+              <li><a class="MenuBarItemSubmenu" href="../../store/sales/purchase-shares.php">Buy Shares</a> </li>
+              
+           <!--   
+              <?php 
+  if (! isset($_SESSION['now_access'])) { $_SESSION['now_access']= 0; }
+  
+if ($_SESSION['now_access'] > 1) { ?>
+              <li><a class="MenuBarItemSubmenu" href="../../store/admin/index.php">Admin</a> </li>
+              <?php } ?>
+               -->
+              
+              
+            </ul>
+            
+            
+          </div>
+        </div>
+      </div></td>
+  </tr>
+</table>
